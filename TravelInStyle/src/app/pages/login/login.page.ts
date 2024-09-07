f37c3a35767d33ae20f1417404a58bc638faff3e
Lala-Banana <belen.avr2001@gmail.com>
2024-09-07 01:46:41 -0400

    Login
    diseno y ruta hacia inicio, faltan verificaciones y otras rutas

--- TravelInStyle/src/app/pages/login/login.page.ts
+++ TravelInStyle/src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { } 
 
   ngOnInit() {
   }
 
+  // Método para manejar el clic en "Entrar"
+  login() {
+    //Poner aqui verificaciones 
+    // ---------------------
+    // Navega a la página de inicio
+    this.router.navigate(['/inicio']);
+  }
 }
