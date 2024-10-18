8f06193e1712ec28bc550a5a936449e6067420c8
Lala-Banana <belen.avr2001@gmail.com>
2024-10-18 08:59:36 -0300

    modificaciones en html de agregar vehiculo

--- TravelInStyle/src/app/pages/agregar-vehiculo/agregar-vehiculo.page.ts
+++ TravelInStyle/src/app/pages/agregar-vehiculo/agregar-vehiculo.page.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Vehiculo } from '../listar-vehiculo/vehiculo';
+import { Camera, CameraResultType } from '@capacitor/camera';
 @Component({
   selector: 'app-agregar-vehiculo',
   templateUrl: './agregar-vehiculo.page.html',
   styleUrls: ['./agregar-vehiculo.page.scss'],
 })
 export class AgregarVehiculoPage implements OnInit {
+  imagen : any;
 
   constructor() { }
 
@@ -28,4 +30,25 @@ export class AgregarVehiculoPage implements OnInit {
     alert("Viaje agregado");
     console.log(this.vehiculo);
   }
+
+  async takePhoto(){
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri
+    });
+    if(image.webPath){
+      const response = await fetch(image.webPath);
+      const blob = await response.blob();
+
+      this.imagen = {
+        fname: 'foto' + image.format,
+        src:image.webPath,
+        file: blob
+      }
+    }
+    var imageUrl = image.webPath;
+    this.imagen.src = imageUrl;
+}
+
 }
---
+++ TravelInStyle/src/app/services/vehiculo.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VehiculoService {
+
+  constructor(private http: HttpClient) { }
+
+  async agregarVehiculo(datoVehiculo:dataBodyVehiculo, imgFileUser:any){
+    try{
+    const formData = new FormData();
+    //formData.append('usuario_id',datoVehiculo.usuario_id);
+    formData.append('p_patente',datoVehiculo.p_patente);
+    formData.append('p_marca',datoVehiculo.p_marca);
+    formData.append('p_modelo',datoVehiculo.p_modelo);  
+    formData.append('p_anio',datoVehiculo.p_anio.toString());
+    formData.append('p_color',datoVehiculo.p_color);
+    formData.append('p_tipo_combustible',datoVehiculo.p_tipo_combustible);
+    formData.append('p_capacidad_pasajeros',datoVehiculo.p_capacidad_pasajeros.toString());  
+
+    if(datoVehiculo.token){
+      formData.append('token',datoVehiculo.token);
+    }
+    formData.append('image_usuario', imgFileUser.file, imgFileUser.name);
+
+    const response = await lastValueFrom(this.http.post<any>(environment.apiUrl + 'vehiculo/agregar',formData));
+    return response;
+  }catch(error){
+    throw error;
+    }
+  }
+}
+
+interface dataBodyVehiculo{
+  p_patente:string;
+  p_marca:string;
+  p_modelo:string;
+  p_anio:number;
+  p_color:string;
+  p_tipo_combustible:string;
+  p_capacidad_pasajeros:number;
+  //usuario_id:string;
+  token?:string;
+}
