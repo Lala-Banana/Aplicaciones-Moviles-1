55d5437ff66c8685aa3776f5605b71d6c08c1f9c
El UN1c0 <133818721+TheN1c0@users.noreply.github.com>
2024-10-22 22:36:59 -0300

    Se agregó el servicio viajes

--- TravelInStyle/src/app/pages/viaje/viaje.page.ts
+++ TravelInStyle/src/app/pages/viaje/viaje.page.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Viaje } from '../lista-viajes/viaje';
 import { AlertController } from '@ionic/angular';
+import { ViajeService } from 'src/app/services/viaje.service';
+import { StorageService } from 'src/app/services/storage.service';
 @Component({
   selector: 'app-viaje',
   templateUrl: './viaje.page.html',
   styleUrls: ['./viaje.page.scss'],
 })
 export class ViajePage implements OnInit {
+  viajes:any[]=[];
 
-  constructor(private alertController: AlertController) { }
+
+  constructor(private alertController: AlertController,
+              private storage:StorageService,
+              private viajeService:ViajeService,
+  ) { }
 
   ngOnInit() {
   }
