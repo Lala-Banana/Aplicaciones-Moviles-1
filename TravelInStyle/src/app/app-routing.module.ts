8d7e23d740140ae620a94ec4f0710094756d3d5b
Lala-Banana <belen.avr2001@gmail.com>
2024-10-27 18:03:48 -0300

    proteccion de rutas
    si inicia sesion la proteccion ya no funciona

--- TravelInStyle/src/app/app-routing.module.ts
+++ TravelInStyle/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
 
+const RedireccionarLogin = () => redirectUnauthorizedTo(['/login']);
 const routes: Routes = [
   {
     path: 'home',
+    canActivate:[AngularFireAuthGuard], data:{authGuardPipe:RedireccionarLogin},
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
@@ -11,12 +14,6 @@ const routes: Routes = [
     redirectTo: 'login',
     pathMatch: 'full'
   },
-  {
-    path: 'home',
-    canActivate:[AngularFireAuthGuard],data:{authGuardPipe:RedireccionarLogin},
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  
   {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
@@ -31,26 +28,32 @@ const routes: Routes = [
   },
   {
     path: 'inicio/:usuario',
+    canActivate:[AngularFireAuthGuard], data:{authGuardPipe:RedireccionarLogin},
     loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
   },
   {
     path: 'viaje',
+    canActivate:[AngularFireAuthGuard], data:{authGuardPipe:RedireccionarLogin},
     loadChildren: () => import('./pages/viaje/viaje.module').then( m => m.ViajePageModule)
   },
   {
     path: 'lista-viajes',
+    canActivate:[AngularFireAuthGuard], data:{authGuardPipe:RedireccionarLogin},
     loadChildren: () => import('./pages/lista-viajes/lista-viajes.module').then( m => m.ListaViajesPageModule)
   },
   {
     path: 'agregar-vehiculo',
+    canActivate:[AngularFireAuthGuard], data:{authGuardPipe:RedireccionarLogin},
     loadChildren: () => import('./pages/agregar-vehiculo/agregar-vehiculo.module').then( m => m.AgregarVehiculoPageModule)
   },
   {
     path: 'listar-vehiculo',
+    canActivate:[AngularFireAuthGuard], data:{authGuardPipe:RedireccionarLogin},
     loadChildren: () => import('./pages/listar-vehiculo/listar-vehiculo.module').then( m => m.ListarVehiculoPageModule)
   },
   {
     path: 'perfil',
+    canActivate:[AngularFireAuthGuard], data:{authGuardPipe:RedireccionarLogin},
     loadChildren: () => import('./pages/perfil/perfil.module').then( m => m.PerfilPageModule)
   },
   {
